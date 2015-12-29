63855d79ad22542d15cd2be8a66e1ecb9b38713c
php-18 <alexander.medina@tektonlabs.com>
2015-12-29 15:07:09 -0500

    Get to Post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,4 +4,4 @@ Route::get('/summoner/{summoner_name}/{region}','Apis\Consume\Riot\RiotSummonerC
 Route::get('/level/{summoner_name}/{region}','Apis\Consume\Riot\RiotSummonerController@findSummonerLevelByName');
 Route::get('/tier/{summoner_name}/{region}','Apis\Consume\Riot\RiotSummonerController@findSummonerTierById');
 Route::get('/champion/{summoner_name}/{region}','Apis\Consume\Riot\RiotSummonerController@findCurrentChampIdBySummonerId');
-Route::get('/slack/response','Apis\Consume\Slack\SlackController@getResponse');
+Route::post('/slack/response','Apis\Consume\Slack\SlackController@getResponse');
